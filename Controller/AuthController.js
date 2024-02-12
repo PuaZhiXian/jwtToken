f02dcbb763c79bcfb1cd01f430600da75996fc85
PuaZhiXian <xianpua2001@gmail.com>
2024-02-12 11:36:46 +0800

    convert login API to sequelize

--- Controller/AuthController.js
+++ Controller/AuthController.js
@@ -30,49 +30,40 @@ module.exports = {
     login: async (req, res, next) => {
         try {
             const result = await authSchema.validateAsync(req.body)
-            const user = await User.findOne({email: result.email})
-            if (!user) throw createError.NotFound(`User not registered`)
 
-            const isMatch = await user.isValidPassword(result.password)
-            if (!isMatch) throw createError.Unauthorized('Username/Password invalid')
+            sequelize.sync()
+                .then(async () => {
 
-            const accessToken = await signAccessToken(user.id)
-            const refreshToken = await signRefreshToken(user.id)
+                    const user = await UserV2.findOne({
+                        where: {email: result.email}
+                    })
+                    if (!user) throw createError.NotFound(`User not registered`)
+                    const isMatch = await user.validPassword(result.password, user.password)
+                    if (!isMatch){
+                        throw createError.NotFound(`Invalid Password`)
+                    }
+                    const accessToken = await signAccessToken(user.id)
+                    const refreshToken = await signRefreshToken(user.id)
+                    res.cookie('accessToken', accessToken, {
+                        httpOnly: true,
+                        secure: true,
+                        sameSite: 'strict',
+                        maxAge: 60 * 60 * 24, // 1 day in seconds
+                        path: '/',
+                    });
 
-            // const serializedAccessToken = cookie.serialize('accessToken', accessToken, {
-            //     httpOnly: true,
-            //     secure: true,
-            //     sameSite: 'strict',
-            //     maxAge: 60 * 60 * 24, // 1 day in seconds
-            //     path: '/',
-            // });
-            //
-            // const serializedRefreshToken = cookie.serialize('refreshToken', refreshToken, {
-            //     httpOnly: true,
-            //     secure: true,
-            //     sameSite: 'strict',
-            //     maxAge: 60 * 60 * 24 * 365,
-            //     path: '/',
-            // });
+                    res.cookie('refreshToken', refreshToken, {
+                        httpOnly: true,
+                        secure: true,
+                        sameSite: 'strict',
+                        maxAge: 60 * 60 * 24 * 365, // 1 year in seconds
+                        path: '/',
+                    });
 
-            res.cookie('accessToken', accessToken, {
-                httpOnly: true,
-                secure: true,
-                sameSite: 'strict',
-                maxAge: 60 * 60 * 24, // 1 day in seconds
-                path: '/',
-            });
-
-            res.cookie('refreshToken', refreshToken, {
-                httpOnly: true,
-                secure: true,
-                sameSite: 'strict',
-                maxAge: 60 * 60 * 24 * 365, // 1 year in seconds
-                path: '/',
-            });
-
-            // res.setHeader('Set-Cookie', [serializedAccessToken, serializedRefreshToken]);
-            res.send({message: 'logged'})
+                    // res.setHeader('Set-Cookie', [serializedAccessToken, serializedRefreshToken]);
+                    res.send({message: 'logged'})
+                })
+                .catch((error) => console.log('Failed to synchronize with the database', error))
         } catch (error) {
             if (error.isJoi === true) return next(createError.BadRequest("Invalid Username/Password"))
             next(error)
@@ -94,7 +85,7 @@ module.exports = {
                     const salt = await bcrypt.genSalt(10)
                     this.password = await bcrypt.hash(result.password, salt)
 
-                    const savedUser  = await UserV2.create({
+                    const savedUser = await UserV2.create({
                         email: result.email,
                         password: this.password
                     })
--- Models/UserV2.model.js
+++ Models/UserV2.model.js
@@ -1,11 +1,27 @@
-const { Sequelize, DataTypes } = require('sequelize')
+const {Sequelize, DataTypes} = require('sequelize')
+const bcrypt = require("bcrypt");
 
 const sequelize = require("../helpers/init_sequelize");
 
 // Define the student model that creates a table in the `student_database`
-const UserV2Model = sequelize.define('user', {
-    email: DataTypes.STRING,
-    password: DataTypes.STRING
-})
+const UserV2Model = sequelize.define('user',
+    {
+        email: DataTypes.STRING,
+        password: DataTypes.STRING
+    },
+    {
+        freezeTableName: true,
+        instanceMethods: {
+            generateHash(password) {
+                return bcrypt.hash(password, bcrypt.genSaltSync(10));
+            },
+            validPassword: (password) => {
+                return bcrypt.compareSync(password, this.password);
+            }
+        }
+    });
+UserV2Model.prototype.validPassword = async (password, hash) => {
+    return await bcrypt.compareSync(password, hash);
+}
 
 module.exports = UserV2Model
